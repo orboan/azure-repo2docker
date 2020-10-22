5bfcd4c992890edcd6a1365b7e6aefb15d399e3e
Oriol Boix Anfosso <orboan@ilg.cat>
2020-10-22 11:07:22 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+USER=training
 DOCKER_COMPOSE_VERSION=1.27.4
 PYTHON_VERSION=3.8
+ANACONDA_INSTALL_SCRIPT=Anaconda3-2020.07-Linux-x86_64.sh
+ANACONDA_URL=https://repo.anaconda.com/archive/$ANACONDA_INSTALL_SCRIPT
 sudo systemctl stop apparmor
 sudo systemctl disable apparmor
 sudo ufw disable
@@ -14,20 +17,22 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update -y
 sudo apt install docker-ce -y
 sudo systemctl enable docker
-sudo useradd -d /home/training -m -s /bin/bash -p $(echo "training" | openssl passwd -1 -stdin) training
-sudo usermod -aG sudo training
-sudo usermod -aG docker training
+sudo useradd -d /home/$USER -m -s /bin/bash -p $(echo "${USER}" | openssl passwd -1 -stdin) $USER
+sudo usermod -aG sudo $USER
+sudo usermod -aG docker $USER
 LN=`sed -n '/%sudo/=' /etc/sudoers
 sudo sed -i "${LN}s/ ALL/ NOPASSWD:ALL/g" /etc/sudoers
 sudo apt install curl -y
 sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt install python${PYTHON_VERSION} -y
-sudo ln -sfn /usr/bin/python${PYTHON_VERSION} /usr/bin/python3
-sudo apt install --reinstall python3-apt -y
-
+sudo su - $USER
+cd /tmp
+curl -O $ANACONDA_URL
+bash $ANACONDA_INSTALL_SCRIPT -b -p $HOME/anaconda
+sudo rm -f $ANACONDA_INSTALL_SCRIPT
+sudo ln -sfn /usr/bin/python3.6 /usr/bin/python3
+eval "$($HOME/anaconda/bin/conda shell.bash hook)"
 
 
 exit 0
